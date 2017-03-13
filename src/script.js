18f730a9ce2aa2ea49e3293c9b8a5f8673e3c418
Gabriel Marcos <gabrielmareia@hotmail.com>
2017-03-13 16:14:35 -0300

    Transform with JS + CSS
    Added the animation when the key are pressed.

--- src/script.js
+++ src/script.js
@@ -1,11 +1,25 @@
 
 window.addEventListener('keydown', function(e){
- var audio = document.querySelector(`.sound [data-key="${e.key}"]`)
+  var audio = document.querySelector(`.sound [data-key="${e.key}"]`)
+  var key = document.querySelector(`.key[data-key="${e.key}"`)
    if(!audio) return;
-
     audio.currentTime = 0;
-    audio.play();
-    key.classList('playing');
-    console.log(audio)
 
+    audio.play();
+    key.classList.add('playing');
+
+
+    setTimeout(function(){
+
+    });
+    function endTransition(e){
+      if(e.propertyName !== 'transform') return;
+      this.classList.remove('playing')
+
+    }
+    /**
+     * @type {NodeList.<HTMLElement>}
+     */
+    var keys = document.querySelectorAll('.key');
+    Array.from(keys).forEach(key => key.addEventListener('transitionend', endTransition));
 });
